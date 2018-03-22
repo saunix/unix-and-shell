0c03448b3b262ce970c2d02916fca31da3f17adc
saunix <35552248+saunix@users.noreply.github.com>
2018-03-22 09:28:29 +0530

    Update sahana_labs4.sh

--- sahana_labs4.sh
+++ sahana_labs4.sh
@@ -1,3 +1,20 @@
+1. write a shell script to display current time and date using date command
+answer:
+echo "date is `date +%D`"
+echo "time is `date +%X`"
+
+
+2.write a shell script to check whether a given number is positive or negative
+answer:
+
+echo "enter a number"
+read n
+if  test $n -gt 0
+then echo "positive number"
+else echo "negative number"
+fi
+
+3.program to check grades
 echo "enter the marks"
 read m
 if [ $m -gt 90 ] 
@@ -16,3 +33,11 @@ then
 else
 	echo "fail"
 fi
+
+6. Replace field "name" with "Students_name" in csea2.csv
+answer:
+sed -i 's/name/Students_name/g' csea2.csv
+cat csea2.csv
+
+7. Sort only name field using positions shell programming
+answer: $cut -d , -f 2 csea2_unix.csv|grep -v "Name"|sort +1 -2
